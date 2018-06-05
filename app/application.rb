9b6a9c89a4e58ef0139669ebbedfdec2ecf1c2c7
Suchitra Naik <susanaik@gmail.com>
2018-06-05 14:10:57 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,10 @@ class Application
 
     if req.path=="/items"
       resp.write "You requested the following items"
+    elsif req.path=="/items/"
+      @@items.each do |item|
+       resp.write "#{item}\n"
+     end
     else
       resp.write "Route not found"
       resp.status = 404
