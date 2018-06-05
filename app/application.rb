d98fc239bb8515b0f0258a7ae2664a0d2ce409ef
Suchitra Naik <susanaik@gmail.com>
2018-06-05 14:23:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,14 +4,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items"
+    if req.path=="/items/"
       resp.write "You requested the following items"
-    elsif req.path=="/items/"
-      @@items.each do |item, price|
-
-       resp.write "#{item} #{price}\n"
-
-     end
     else
       resp.write "Route not found"
       resp.status = 404
