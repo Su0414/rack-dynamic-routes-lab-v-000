4ef26ffa5761df519117393534c1218fc0ffdec1
Suchitra Naik <susanaik@gmail.com>
2018-06-05 14:35:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,12 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items/"
-      resp.write "You requested the following items"
-      @@items.each do |item|
-        resp.write "#{item}\n"
+    if req.path.match(/items/)
+
+        item_name = req.path.split("/items/").last
+        item = @@items.find{|i| i.name == item_name}
+
+        resp.write item.price
       end
 
     else
